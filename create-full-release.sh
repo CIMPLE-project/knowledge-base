3736b6b5c06e68f660fba86e723f288733711809
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2024-01-19 12:27:50 +0100

    Add a script to create a full release

---
+++ create-full-release.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# create a new release
+# user: user's name
+# repo: the repo's name
+# token: github api user token
+# tag: name of the tag pushed
+create_release() {
+    user=$1
+    repo=$2
+    token=$3
+    tag=$4
+    name=$5
+
+    command="curl -s -o release.json -w '%{http_code}' \
+         --request POST \
+         --header 'authorization: Bearer ${token}' \
+         --header 'content-type: application/json' \
+         --data '{\"tag_name\": \"${tag}\", \"name\": \"${name}\", \"body\": \"Full release from ${name}\" }' \
+         https://api.github.com/repos/$user/$repo/releases"
+    http_code=`eval $command`
+    if [ $http_code == "201" ]; then
+        echo "created release:"
+        cat release.json
+    else
+        echo "create release failed with code '$http_code':"
+        cat release.json
+        echo "command:"
+        echo $command
+        return 1
+    fi
+}
+
+# upload a release file.
+# this must be called only after a successful create_release, as create_release saves
+# the json response in release.json.
+# token: github api user token
+# file: path to the asset file to upload
+# name: name to use for the uploaded asset
+upload_release_file() {
+    token=$1
+    file=$2
+    name=$3
+
+    url=`jq -r .upload_url release.json | cut -d{ -f'1'`
+    command="\
+      curl -s -o upload.json -w '%{http_code}' \
+           --request POST \
+           --header 'authorization: Bearer ${token}' \
+           --header 'Content-Type: application/octet-stream' \
+           --data-binary @\"${file}\"
+           ${url}?name=${name}"
+    http_code=`eval $command`
+    if [ $http_code == "201" ]; then
+        echo "asset $name uploaded:"
+        jq -r .browser_download_url upload.json
+    else
+        echo "upload failed with code '$http_code':"
+        cat upload.json
+        echo "command:"
+        echo $command
+        return 1
+    fi
+}
+
+# Make sure a path was passed in
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <path to directory to compress>"
+    exit 1
+fi
+
+# Make sure the path exists
+if [ ! -d "$1" ]; then
+    echo "Error: $1 is not a directory."
+    exit 1
+fi
+
+# Check for valid github token
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "Error: GITHUB_TOKEN is not set."
+    exit 1
+fi
+
+# Create a .tar.gz file and upload it into a new release
+dt=$(date '+%Y-%m-%d')
+compressed_file="${dt}.tar.gz"
+echo "Compressing ${1} into ${compressed_file}"
+tar -czvf "${compressed_file}" -C "${1}" .
+if create_release "CIMPLE-project" "knowledge-base" "${GITHUB_TOKEN}" "${dt}" "${dt}" ; then
+    upload_release_file "${GITHUB_TOKEN}" "${compressed_file}" "${compressed_file}"
+    rm "upload.json"
+    rm "release.json"
+fi
+rm "${compressed_file}"
