1846c653456c7ca9b1942a4a5cb1d2443c8e6c1e
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2024-07-24 10:57:39 +0200

    create-release: fix http requests and improve logging/files cleaning

--- webhookd/scripts/create-release.sh
+++ webhookd/scripts/create-release.sh
@@ -12,20 +12,19 @@ create_release() {
     tag=$4
     name=$5
 
-    command="curl -s -o release.json -w '%{http_code}' \
-         --request POST \
-         --header 'authorization: Bearer ${token}' \
-         --header 'content-type: application/json' \
-         --data '{\"tag_name\": \"${tag}\", \"name\": \"${name}\", \"body\": \"Daily release from ${name}\" }' \
-         https://api.github.com/repos/$user/$repo/releases"
-    http_code=$(eval "${command}")
+    http_code=$(curl -s -o release.json -w '%{http_code}' \
+           --request POST \
+           --header "Authorization: Bearer ${token}" \
+           --header "Content-Type: application/octet-stream" \
+           --data '{\"tag_name\": \"${tag}\", \"name\": \"${name}\", \"body\": \"Daily release from ${name}\" }' \
+           "https://api.github.com/repos/$user/$repo/releases")
     if [ "${http_code}" = "201" ]; then
-        echo "created release:"
+        echo "[CREATE-RELEASE] Created release:"
         cat release.json
     else
-        echo "create release failed with code '${http_code}':"
+        echo "[CREATE-RELEASE] Create release failed with code '${http_code}':"
         cat release.json
-        echo "command: ${command}"
+        echo "[CREATE-RELEASE] Command: ${command}"
         return 1
     fi
 }
@@ -41,45 +40,44 @@ upload_release_file() {
     file=$2
     name=$3
 
-    url=$(jq -r .upload_url release.json | cut -d'{' -f1)
-    command="\
-      curl -s -o upload.json -w '%{http_code}' \
+    url=$(jq -r .upload_url release.json | cut -d'{' -f1 | sed "s/{?name,label}/?name=${name}/")
+    http_code=$(curl -s -o upload.json -w '%{http_code}' \
            --request POST \
-           --header 'authorization: Bearer ${token}' \
-           --header 'Content-Type: application/octet-stream' \
-           --data-binary @\"${file}\"
-           ${url}?name=${name}"
-    http_code=$(eval "${command}")
+           --header "Authorization: Bearer ${token}" \
+           --header "Content-Type: application/octet-stream" \
+           --data-binary @"${file}" \
+           "${url}")
     if [ "${http_code}" = "201" ]; then
-        echo "asset ${name} uploaded:"
+        echo "[CREATE-RELEASE] Asset ${name} uploaded:"
         jq -r .browser_download_url upload.json
     else
-        echo "upload failed with code '${http_code}':"
+        echo "[CREATE-RELEASE] Upload failed with code '${http_code}':"
         cat upload.json
-        echo "command: ${command}"
+        echo "[CREATE-RELEASE] Command: ${command}"
         return 1
     fi
 }
 
 # Check for parameters
 if [ -z "${1}" ]; then
-    echo "Missing parameter: file_path"
+    echo "[CREATE-RELEASE] Missing parameter: file_path"
     exit 1
 fi
 if [ -z "${2}" ]; then
-    echo "Missing parameter: release_name"
+    echo "[CREATE-RELEASE] Missing parameter: release_name"
     exit 1
 fi
 
 # Create a gzip file and upload it into a new release
 release_name="${2}"
-filename=$(basename "${1}")
-compressed_file="${filename}.gz"
-echo "Compressing ${1} into ${compressed_file}"
+compressed_file="${1}.gz"
+echo "[CREATE-RELEASE] Compressing ${1} into ${compressed_file}"
 gzip -c "${1}" > "${compressed_file}"
 if create_release "CIMPLE-project" "knowledge-base" "${GITHUB_TOKEN}" "${release_name}" "${release_name}" ; then
     upload_release_file "${GITHUB_TOKEN}" "${compressed_file}" "${compressed_file}"
-    rm "upload.json"
-    rm "release.json"
 fi
-rm "${compressed_file}"
+
+echo "[CREATE-RELEASE] Cleaning up..."
+rm -f "${compressed_file}"
+rm -f "upload.json"
+rm -f "release.json"
